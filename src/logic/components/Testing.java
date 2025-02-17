bb668af29c5f9b4ffa916b478cc583a6839f8197
Tananan Narkchuay <zackalocloud@gmail.com>
2025-02-17 17:28:08 +0700

    add 555

--- src/logic/components/Testing.java
+++ src/logic/components/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("new test 555");
+		System.out.println("new test 55555555555");
 		System.out.print(true);
 	}
 
