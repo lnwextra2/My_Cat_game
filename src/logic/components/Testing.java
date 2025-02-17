63415bc82c78fd2924c7d4e1c99f867714cf7ef2
TeeRatTaPon27 <teerattapon080849@gmail.com>
2025-02-17 16:57:16 +0700

    Update message "Haha 007" to "Haha 007 V2"

--- src/logic/components/Testing.java
+++ src/logic/components/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Haha 007");
+		System.out.println("Haha 007 V2");
 		System.out.println("Test Commit in eclipse");
 	}
 
